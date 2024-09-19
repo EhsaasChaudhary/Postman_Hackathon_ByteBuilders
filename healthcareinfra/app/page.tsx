d50b07465afb1da593a889ff5d2def603d1d14de
EhsaasChaudhary <116794955+EhsaasChaudhary@users.noreply.github.com>
2024-09-19 15:28:28 +0530

    sidebar done

--- healthcareinfra/app/page.tsx
+++ healthcareinfra/app/page.tsx
@@ -12,7 +12,7 @@ import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
 
-export function SidebarDemo() {
+export default function SidebarDemo() {
   const links = [
     {
       label: "Dashboard",
@@ -46,10 +46,11 @@ export function SidebarDemo() {
   const [open, setOpen] = useState(false);
   return (
     <div
-      className={cn(
-        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 max-w-7xl mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
-        "h-[60vh]" // for your use case, use `h-screen` instead of `h-[60vh]`
-      )}
+    className={cn(
+      "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-screen flex-1 mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+      "h-screen"
+    )}
+    
     >
       <Sidebar open={open} setOpen={setOpen}>
         <SidebarBody className="justify-between gap-10">
